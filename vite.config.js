3ffd306f8ceb9b06d69327cb447d624f362df186
Davida <byourself77by@gmail.com>
2024-10-19 09:38:13 +0000

    Change configuration

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 3500,
+  },
+  build: {
+    rollupOptions: {
+      external: ['react-icons/fa']
+    }
   }
-})
+});
